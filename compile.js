3f412a08b3e2012edf505d7210ce9ca275cb7650
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2011-03-06 18:30:52 +0100

    ...

--- compile.js
+++ compile.js
@@ -1,5 +1,12 @@
+//
 // Forth-like-language that compiles to small (< 1K) JavaScript files.
+//
+function trim(s) {
+      return s.replace(/^\s*(\S*(\s+\S+)*)\s*$/, "$1");
+};
+
 fs = require("fs");
+_ = require("underscore");
 if(process.argv.length != 3 
   || process.argv[2].slice(-4) !== ".f1k") {
     console.log("Usage: node compile.js $infile.f1k");
@@ -8,15 +15,62 @@ if(process.argv.length != 3
 
 lines = fs.readFileSync(process.argv[2], "utf8").split("\n");
 
+fn = {};
+
+for(i=0;i<lines.length;++i) {
+    line = trim(lines[i]);
+    if(line === "" || line.slice(0,2) === "//") {
+        continue;
+    }
+
+    pos = 0;
+    while(line[pos] != ' ') {
+        ++pos;
+    }
+    name = line.slice(0,pos);
+    fn[name] = [];
+    while(line[pos] === ' ') ++pos;
+
+    while(pos < line.length) {
+        pos0 = pos;
+        if(line[pos] === '"') {
+            ++pos;
+            while(pos<line.length && line[pos] !== '"') {
+                ++pos;
+            }
+            ++pos;
+            fn[name].push(["string", line.slice(pos0, pos)]);
+        } else if(line[pos] === '$') {
+            while(pos < line.length && line[pos] !== ' ') { ++pos; }
+            fn[name].push(["builtin", parseInt(line.slice(pos0+1))]);
+        } else if('0' <= line[pos] && line[pos] <= '9') {
+            while(pos < line.length && line[pos] !== ' ') { ++pos; }
+            fn[name].push(["num", parseInt(line.slice(pos0))]);
+        } else if(line[pos] === "'") {
+            while(pos < line.length && line[pos] !== ' ') {
+                ++pos;
+            }
+            fn[name].push(["quote", line.slice(pos0+1, pos)]);
+        } else {
+            while(pos < line.length && line[pos] !== ' ') {
+                ++pos;
+            }
+            fn[name].push(["call", line.slice(pos0, pos)]);
+        }
+        while(line[pos] === ' ') ++pos;
+    }
+}
+console.log(fn);
+
 //
 // Code generator
 //
-codestr = '\x02\x01\x05~console.log("Hello world")';
+codestr = "\x02\x01\x05~console.log('Hello world')";
 fncount = 1;
 splitsymb = "~";
 interpreter = fs.readFileSync("interpreter.js", "utf8");
-code = interpreter.replace("$CODESTR", "'" + codestr + "'");
-code = code.replace("$SPLITSYMB", "'" + splitsymb + "'");
+code = interpreter.replace("$CODESTR", '"' + codestr + '"');
+code = code.replace("$SPLITSYMB", '"' + splitsymb + '"');
 code = code.replace("$FNCOUNT", fncount);
 
 fs.writeFileSync("f1k.out.js", code, "utf8");
--- interpreter.js
+++ interpreter.js
@@ -1,9 +1,1 @@
-function X(c) {
-    for(i=0;o=S[c].charCodeAt(i++);o&2?A.push(o&1?n:S[n+$FNCOUNT]):o&1?J[n]():X(n)) {
-        n=o>>2;
-    }
-}
-A=[];
-J=[function(){J.push(Function(A.join('')))}];
-S=$CODESTR.split($SPLITSYMB);
-X(0)
+function X(c){for(i=0;o=S[c].charCodeAt(i++);o&2?A.push(o&1?n:S[n+$FNCOUNT]):o&1?J[n]():X(n))n=o>>2}A=[];J=[function(){J.push(Function(A.join('')));A=[]}];S=$CODESTR.split($SPLITSYMB);X(0)
