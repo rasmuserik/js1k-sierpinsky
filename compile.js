6d7319de15414edef0c264c4eb020d64d7df78bd
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2011-03-06 19:32:31 +0100

    fixed indent

--- compile.js
+++ compile.js
@@ -2,13 +2,13 @@
 // Forth-like-language that compiles to small (< 1K) JavaScript files.
 //
 function trim(s) {
-      return s.replace(/^\s*(\S*(\s+\S+)*)\s*$/, "$1");
+    return s.replace(/^\s*(\S*(\s+\S+)*)\s*$/, "$1");
 };
 
 fs = require("fs");
 _ = require("underscore");
-if(process.argv.length != 3 
-  || process.argv[2].slice(-4) !== ".f1k") {
+if (process.argv.length != 3
+        || process.argv[2].slice(-4) !== ".f1k") {
     console.log("Usage: node compile.js $infile.f1k");
     process.exit();
 }
@@ -17,53 +17,57 @@ lines = fs.readFileSync(process.argv[2], "utf8").split("\n");
 
 fn = {};
 
-for(i=0;i<lines.length;++i) {
+for (i=0;i<lines.length;++i) {
     line = trim(lines[i]);
-    if(line === "" || line.slice(0,2) === "//") {
+    if (line === "" || line.slice(0,2) === "//") {
         continue;
     }
 
     pos = 0;
-    while(line[pos] != ' ') {
+    while (line[pos] != ' ') {
         ++pos;
     }
     name = line.slice(0,pos);
-    while(line[pos] === ' ') ++pos;
+    while (line[pos] === ' ') ++pos;
 
-  function readDef() {
-      var result, pos0; // pos and line is in external scope
-      result = [];
-    while(pos < line.length) {
-        pos0 = pos;
-        if(line[pos] === '"') {
-            ++pos;
-            while(pos<line.length && line[pos] !== '"') {
+    function readDef() {
+        var result, pos0; // pos and line is in external scope
+        result = [];
+        while (pos < line.length) {
+            pos0 = pos;
+            if (line[pos] === '"') {
                 ++pos;
-            }
-            ++pos;
-            result.push(["string", line.slice(pos0+1, pos-1)]);
-        } else if(line[pos] === '$') {
-            while(pos < line.length && line[pos] !== ' ') { ++pos; }
-            result.push(["builtin", parseInt(line.slice(pos0+1))]);
-        } else if('0' <= line[pos] && line[pos] <= '9') {
-            while(pos < line.length && line[pos] !== ' ') { ++pos; }
-            result.push(["num", parseInt(line.slice(pos0))]);
-        } else if(line[pos] === "'") {
-            while(pos < line.length && line[pos] !== ' ') {
+                while (pos<line.length && line[pos] !== '"') {
+                    ++pos;
+                }
                 ++pos;
+                result.push(["string", line.slice(pos0+1, pos-1)]);
+            } else if (line[pos] === '$') {
+                while (pos < line.length && line[pos] !== ' ') {
+                    ++pos;
+                }
+                result.push(["builtin", parseInt(line.slice(pos0+1))]);
+            } else if ('0' <= line[pos] && line[pos] <= '9') {
+                while (pos < line.length && line[pos] !== ' ') {
+                    ++pos;
+                }
+                result.push(["num", parseInt(line.slice(pos0))]);
+            } else if (line[pos] === "'") {
+                while (pos < line.length && line[pos] !== ' ') {
+                    ++pos;
+                }
+                result.push(["quote", line.slice(pos0+1, pos)]);
+            } else {
+                while (pos < line.length && line[pos] !== ' ') {
+                    ++pos;
+                }
+                result.push(["call", line.slice(pos0, pos)]);
             }
-            result.push(["quote", line.slice(pos0+1, pos)]);
-        } else {
-            while(pos < line.length && line[pos] !== ' ') {
-                ++pos;
-            }
-            result.push(["call", line.slice(pos0, pos)]);
+            while (line[pos] === ' ') ++pos;
         }
-        while(line[pos] === ' ') ++pos;
+        return result;
     }
-    return result;
-  }
-  fn[name] = readDef();
+    fn[name] = readDef();
 }
 
 
@@ -73,7 +77,7 @@ for(i=0;i<lines.length;++i) {
 //
 
 var i = 0;
-for(x in fn) {
+for (x in fn) {
     fn[x].id = i;
     ++i;
 }
@@ -81,41 +85,46 @@ fncount = i;
 
 strings = [];
 code = [];
-for(key in fn) {
+for (key in fn) {
     x = fn[key];
     var s = "";
-    for(i = 0; i < x.length; ++i) {
+    for (i = 0; i < x.length; ++i) {
         var op;
-        if(x[i][0] === "call") {
-            op = 0; n = fn[x[i][1]].id;
-            if(n === undefined) {
+        if (x[i][0] === "call") {
+            op = 0;
+            n = fn[x[i][1]].id;
+            if (n === undefined) {
                 console.log("could not find function: " + fn[i][1] + " while compiling " + x);
             }
-        } else if(x[i][0] === "quote") {
-            op = 3; n = fn[x[i][1]].id;
-            if(n === undefined) {
+        } else if (x[i][0] === "quote") {
+            op = 3;
+            n = fn[x[i][1]].id;
+            if (n === undefined) {
                 console.log("could not find function: " + fn[i][1] + " while compiling " + x);
             }
-        } else if(x[i][0] === "builtin") {
-            op = 1; n = x[i][1];
-        } else if(x[i][0] === "string") {
+        } else if (x[i][0] === "builtin") {
+            op = 1;
+            n = x[i][1];
+        } else if (x[i][0] === "string") {
             var str = x[i][1];
-            if(strings[str]) {
+            if (strings[str]) {
                 string_id = strings[str].id;
             } else {
                 string_id = strings.length;
-                strings[str] = {id: string_id};
+strings[str] = {id:
+                                string_id};
                 str = str.replace("\n", "\\n").replace("\t", "\\t").replace("\"", "\\\"").replace("\\","\\\\");
                 strings.push(str);
             }
             op = 2;
             n = string_id;
-        } else if(x[i][0] === "num") {
-            op = 3; n = x[i][1];
+        } else if (x[i][0] === "num") {
+            op = 3;
+            n = x[i][1];
         } else {
             console.log("unexpected node type: " + x[i][0]);
         }
-        console.log({op: op, n: n});
+console.log( {op: op, n: n});
         s += String.fromCharCode(op + 4*n);
     }
     code.push(x.src = s);
