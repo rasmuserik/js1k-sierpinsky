dae766c1697b5653f4d618d8ae2ef3af599d95c2
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2011-03-06 16:01:50 +0100

    ...

--- compile.js
+++ compile.js
@@ -1,18 +1,24 @@
 // Forth-like-language that compiles to small (< 1K) JavaScript files.
 fs = require("fs");
-if(process.argv.length != 4 
-  || process.argv[2].slice(-4) !== ".f1k"
-  || process.argv[3].slice(-3) !== ".js") {
-    console.log("Usage: node compile.js $infile.f1k $outfile.js");
+if(process.argv.length != 3 
+  || process.argv[2].slice(-4) !== ".f1k") {
+    console.log("Usage: node compile.js $infile.f1k");
     process.exit();
 }
 
-c = fs.readFileSync(process.argv[2], "utf8");
+lines = fs.readFileSync(process.argv[2], "utf8").split("\n");
 
-codestr = c;
-fncount = 3;
+//
+// Code generator
+//
+codestr = '\x02\x01\x05~console.log("Hello world")';
+fncount = 1;
 splitsymb = "~";
-codestr = "function X(c){for(i=0;o=S[c][i++];o&2?A.push(o&1?n:S[n+" + fncount + "]):o&1?J[n]():X(n))n=o>>2}A=[];J=[function(){J.push(Function(A.join('')))}];S='" + codestr + "'.split('" + splitsymb + "');X(0)";
-fs.writeFileSync(process.argv[3], codestr, "utf8");
+interpreter = fs.readFileSync("interpreter.js", "utf8");
+code = interpreter.replace("$CODESTR", "'" + codestr + "'");
+code = code.replace("$SPLITSYMB", "'" + splitsymb + "'");
+code = code.replace("$FNCOUNT", fncount);
 
-fs.writeFileSync(process.argv[3] + ".html", '<!doctype html><html> <head> <title>JS1k, 1k demo submission [ID]</title> <meta charset="utf-8" /> </head> <body> <canvas id="c"></canvas> <script> var b = document.body; var c = document.getElementsByTagName(\'canvas\')[0]; var a = c.getContext(\'2d\'); document.body.clientWidth; // fix bug in webkit: http://qfox.nl/weblog/218 </script> <script> // start of submission //\n" + codestr + "\n// end of submission // </script> </body> </html>', "utf8");
+fs.writeFileSync("f1k.out.js", code, "utf8");
+
+fs.writeFileSync("f1k.out.html", '<!doctype html><html> <head> <title>JS1k, 1k demo submission [ID]</title> <meta charset="utf-8" /> </head> <body> <canvas id="c"></canvas> <script> var b = document.body; var c = document.getElementsByTagName(\'canvas\')[0]; var a = c.getContext(\'2d\'); document.body.clientWidth; // fix bug in webkit: http://qfox.nl/weblog/218 </script> <script> // start of submission //\n' + code + '\n// end of submission // </script> </body> </html>', "utf8");
