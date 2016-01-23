44d327290e14019fcb58dd707d80ae5e80a21259
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-01-23 09:09:05 +0100

    separate repository

--- interpreter.js
+++
@@ -1,1 +0,0 @@
-function X(c,i,n){for(i=0;o=S[c].charCodeAt(i++);o&2?A.push(o&1?n:S[n+$FNCOUNT]):o&1?J[n]():X(n))n=o>>2}A=[];J=[function(){J.push(Function(A.join('')));A=[]}];S=$CODESTR.split($SPLITSYMB);X(0)
